2e55de76b99d55472c06d3ef27bf5505d81df814
Adarsh Kumar <adarsh.kumar2@flipkart.com>
2024-09-04 15:06:06 +0530

    updated beans

--- JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/bean/Notifications.java
+++ JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/bean/Notifications.java
@@ -1,4 +1,37 @@
 package com.flipkart.bean;
 
+/**
+ * This class represents notifications in the GymFlipFit application.
+ * It stores information about the notification's category, message, and notification ID.
+ */
+
 public class Notifications {
+
+    private String category; // Category of the notification
+    private String message; // Message content of the notification
+    private int notificationId; // Unique identifier for the notification
+
+    public int getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(int notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/bean/Slots.java
+++ JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/bean/Slots.java
@@ -1,4 +1,43 @@
 package com.flipkart.bean;
 
+/**
+ * This class represents a time slot for gym bookings in the GymFlipFit application.
+ * It stores information about the slot ID, start time, and available seat count.
+ */
+
 public class Slots {
+
+    private int slotsId; // Unique identifier for the slot
+    private int startTime; // Start time of the slot
+    private int seatCount; // Number of available seats in the slot
+
+    public Slots(int slotsId, int startTime, int seatCount) {
+        this.setSlotsId(slotsId);
+        this.setStartTime(startTime);
+        this.setSeatCount(seatCount);
+    }
+
+    public int getSlotsId() {
+        return slotsId;
+    }
+
+    public void setSlotsId(int slotsId) {
+        this.slotsId = slotsId;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getSeatCount() {
+        return seatCount;
+    }
+
+    public void setSeatCount(int seatCount) {
+        this.seatCount = seatCount;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
 }
