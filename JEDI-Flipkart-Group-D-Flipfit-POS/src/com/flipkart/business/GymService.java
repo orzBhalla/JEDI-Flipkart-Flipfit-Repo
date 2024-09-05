d39de510ba09b343416c328c9c789580829aab26
karia.shreyachirag <karia.shreyachirag@flipkart.com>
2024-09-05 22:14:25 +0530

    Gym Service operations added

--- JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/business/GymService.java
+++ JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/business/GymService.java
@@ -1,4 +1,19 @@
 package com.flipkart.business;
 
+import com.flipkart.bean.Gym;
+
+import java.util.Map;
+
 public interface GymService {
+
+    void createGym(Gym gym);
+
+    void listAllGyms();
+
+    void listAllGymsWithArea(String areaName);
+
+    public static Map<Integer, Gym> getGymMap() {
+        return Map.of();
+    }
+
 }
--- JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/business/GymServiceOperations.java
+++ JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/business/GymServiceOperations.java
@@ -1,4 +1,53 @@
 package com.flipkart.business;
 
-public class GymServiceOperations {
+import com.flipkart.bean.Gym;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GymServiceOperations implements GymService {
+
+    static HashMap<Integer, Gym> gymMap = new HashMap<>();
+    int gymIdCounter = 0;
+
+    @Override
+    public void createGym(Gym gym) {
+        if (gymMap.containsKey(gym.getGymId())) {
+            return;
+        }
+        gym.setGymId(++gymIdCounter);
+        gymMap.put(gym.getGymId(), gym);
+
+    }
+
+    @Override
+    public void listAllGyms() {
+        for (Gym gym : gymMap.values()) {
+            System.out.println("Gym ID: " + gym.getGymId());
+            System.out.println("Name: " + gym.getGymName());
+            System.out.println("Address " + gym.getGymAddress());
+            System.out.println("Location: " +gym.getLocation());
+            System.out.println("Owner ID: " + gym.getOwnerId());
+            System.out.println("Status: " + gym.getStatus());
+        }
+    }
+
+    @Override
+    public void listAllGymsWithArea(String areaName) {
+        for (Gym gym : gymMap.values()) {
+            if (gym.getGymAddress().contains(areaName)) {
+                System.out.println("Gym ID: " + gym.getGymId());
+                System.out.println("Name: " + gym.getGymName());
+                System.out.println("Address " + gym.getGymAddress());
+                System.out.println("Location: " +gym.getLocation());
+                System.out.println("Owner ID: " + gym.getOwnerId());
+                System.out.println("Status: " + gym.getStatus());
+            }
+        }
+    }
+
+    public static Map<Integer, Gym> getGymMap() {
+        return gymMap;
+    }
+
 }
