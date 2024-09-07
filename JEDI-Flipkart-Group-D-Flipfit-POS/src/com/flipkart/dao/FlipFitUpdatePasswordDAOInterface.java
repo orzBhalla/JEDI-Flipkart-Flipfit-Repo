9e816953942240d8592cfa14661219ee7accbeef
Nitish Kumar <nitishkumarsher2002@gmail.com>
2024-09-07 10:12:02 +0530

    updatepasswordDao

---
+++ JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/dao/FlipFitUpdatePasswordDAOInterface.java
@@ -0,0 +1,15 @@
+package com.flipkart.dao;
+
+import com.flipkart.bean.GymOwner;
+import com.flipkart.bean.User;
+
+public interface FlipFitUpdatePasswordDAOInterface {
+
+    public void updateGymOwnerPassword(String email, String password, String updatedPassword);
+
+    public void updateGymUserPassword(String email, String password, String updatedPassword);
+
+    public boolean verifyGymOwnerPassword(String email, String password);
+
+    public boolean verifyGymUserPassword(String email, String password);
+}
