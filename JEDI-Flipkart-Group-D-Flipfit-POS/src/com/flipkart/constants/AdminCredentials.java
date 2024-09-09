4f2e110d0f3ec3182ba8aa9a811a54a17613bd46
chahat.biswas <chahat.biswas@flipkart.com>
2024-09-09 09:00:22 -0700

    comment for AdminCredentials.java comment for ColorConstants.java comment for SQLConstants.java

--- JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/constants/AdminCredentials.java
+++ JEDI-Flipkart-Group-D-Flipfit-POS/src/com/flipkart/constants/AdminCredentials.java
@@ -1,6 +1,16 @@
 package com.flipkart.constants;
 
+/**
+ * Class to hold the constant values for admin credentials.
+ * This class provides the email and password for admin authentication.
+ *
+ * @author Nitish and Navaratna
+ */
 public class AdminCredentials {
-    public static final String ADMIN_EMAIL = "admin@flipkart.com";
-    public static final String ADMIN_PASSWORD = "admin";
+
+    // Admin email address
+    public static final String ADMIN_EMAIL = "adarsh@flipkart.com";
+
+    // Admin password
+    public static final String ADMIN_PASSWORD = "adarsh";
 }
