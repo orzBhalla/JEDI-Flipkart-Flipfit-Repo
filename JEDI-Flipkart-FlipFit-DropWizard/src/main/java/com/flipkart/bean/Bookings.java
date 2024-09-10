2e7cf7b65ca73afe195fa0b228cfbf9830ca4646
Nitish Kumar <nitishkumarsher2002@gmail.com>
2024-09-10 14:45:40 +0530

    Bean

--- JEDI-Flipkart-FlipFit-DropWizard/src/main/java/com/flipkart/bean/Bookings.java
+++ JEDI-Flipkart-FlipFit-DropWizard/src/main/java/com/flipkart/bean/Bookings.java
@@ -1,10 +1,17 @@
 package com.flipkart.bean;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Represents a booking in the system. Contains details about the booking including the user, date, time, and status.
  *
  * @author Adarsh, Shriya
  */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Bookings {
 
     /**
@@ -52,167 +59,6 @@ public class Bookings {
      */
     private String status;
 
-    /**
-     * Gets the date of the booking.
-     *
-     * @return the date of the booking
-     */
-    public int getDate() {
-        return date;
-    }
-
-    /**
-     * Sets the date of the booking.
-     *
-     * @param date the date of the booking
-     */
-    public void setDate(int date) {
-        this.date = date;
-    }
-
-    /**
-     * Gets the slot ID for the booking.
-     *
-     * @return the slot ID for the booking
-     */
-    public int getSlotId() {
-        return slotId;
-    }
-
-    /**
-     * Sets the slot ID for the booking.
-     *
-     * @param slotId the slot ID for the booking
-     */
-    public void setSlotId(int slotId) {
-        this.slotId = slotId;
-    }
-
-    /**
-     * Gets the gym ID for the booking.
-     *
-     * @return the gym ID for the booking
-     */
-    public int getGymId() {
-        return gymId;
-    }
-
-    /**
-     * Sets the gym ID for the booking.
-     *
-     * @param gymId the gym ID for the booking
-     */
-    public void setGymId(int gymId) {
-        this.gymId = gymId;
-    }
-
-    /**
-     * Gets the status of the booking as a string.
-     *
-     * @return the status of the booking
-     */
-    public String getStatus() {
-        return status;
-    }
-
-    /**
-     * Sets the status of the booking as a string.
-     *
-     * @param status the status of the booking
-     */
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    /**
-     * Gets the time of the booking.
-     *
-     * @return the time of the booking
-     */
-    public int getTime() {
-        return time;
-    }
-
-    /**
-     * Sets the time of the booking.
-     *
-     * @param time the time of the booking
-     */
-    public void setTime(int time) {
-        this.time = time;
-    }
-
-    /**
-     * Gets the booking ID.
-     *
-     * @return the booking ID
-     */
-    public int getBookingId() {
-        return bookingId;
-    }
-
-    /**
-     * Sets the booking ID.
-     *
-     * @param bookingId the booking ID
-     */
-    public void setBookingId(int bookingId) {
-        this.bookingId = bookingId;
-    }
-
-    /**
-     * Gets the user ID for the booking.
-     *
-     * @return the user ID for the booking
-     */
-    public int getUserId() {
-        return userId;
-    }
-
-    /**
-     * Sets the user ID for the booking.
-     *
-     * @param userId the user ID for the booking
-     */
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    /**
-     * Gets the timestamp of when the booking was created.
-     *
-     * @return the timestamp of when the booking was created
-     */
-    public int getCreatedAt() {
-        return createdAt;
-    }
-
-    /**
-     * Sets the timestamp of when the booking was created.
-     *
-     * @param createdAt the timestamp of when the booking was created
-     */
-    public void setCreatedAt(int createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    /**
-     * Gets the booking status.
-     *
-     * @return the booking status
-     */
-    public int getBookingStatus() {
-        return bookingStatus;
-    }
-
-    /**
-     * Sets the booking status.
-     *
-     * @param bookingStatus the booking status
-     */
-    public void setBookingStatus(int bookingStatus) {
-        this.bookingStatus = bookingStatus;
-    }
 }
 
 //package com.flipkart.bean;
--- JEDI-Flipkart-FlipFit-DropWizard/src/main/java/com/flipkart/bean/Gym.java
+++ JEDI-Flipkart-FlipFit-DropWizard/src/main/java/com/flipkart/bean/Gym.java
@@ -1,4 +1,27 @@
 package com.flipkart.bean;
 
+package com.flipkart.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Represents a gym facility with various attributes.
+ * @author Atulya, Avinash
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Gym {
+
+    private int gymId;
+    private String gymName;
+    private String gymAddress;
+    private String location;
+    private List<Slots> slots;
+    private String ownerId;
+    private String Status;
 }
