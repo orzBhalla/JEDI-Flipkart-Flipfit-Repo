3c262be80bbbe79cbd2e803c166efb01bfe6107c
Shriya Pandey <shriya.pandey@flipkart.com>
2024-09-10 16:27:20 +0530

    Server working

--- JEDI-Flipkart-FlipFit-DropWizard/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.flipkart.app"},{"l":"com.flipkart.bean"},{"l":"com.flipkart.business"},{"l":"com.flipkart.constants"},{"l":"com.flipkart.dao"},{"l":"com.flipkart.exception"},{"l":"com.flipkart.rest"},{"l":"com.flipkart.utils"}];updateSearchResults();
