91083d68cbb5346e7dd2f01cda87707744b47ec3
Anshul Kashyap <anshul.kashyap@fk10890-225510l.fkipl.flipkart.com>
2024-10-10 09:47:26 +0530

    Added menu for customer

--- JEDI-Flipfit-Java-POS/src/com/flipkart/client/GymFlipFitCustomerMenu.java
+++ JEDI-Flipfit-Java-POS/src/com/flipkart/client/GymFlipFitCustomerMenu.java
@@ -2,8 +2,14 @@ package com.flipkart.client;
 
 public class GymFlipFitCustomerMenu {
     public static void displayCustomerMenu() {
-        System.out.println("1. View Gym Schedule");
+        System.out.println("1. Registeration");
         System.out.println("2. Update Profile");
+        System.out.println("3. View Gym Schedule");
+        System.out.println("4. View All Gym Centres");
+        System.out.println("5. Book Slot");
+        System.out.println("6. Cancel Slot");
+        System.out.println("7. Exit");
+
         // Add more customer options as needed
     }
 }
