580810279fe7e8d7b830b558b8ce56ee4d4ec73d
akanksha042 <87239078+akanksha042@users.noreply.github.com>
2024-10-14 10:12:58 +0530

    Delete JEDI-Flipfit-Java-POS directory

--- JEDI-Flipfit-Java-POS/src/com/flipkart/bean/FlipFitRole.java
+++
@@ -1,7 +0,0 @@
-package com.flipkart.bean;
-
-public enum FlipFitRole {
-    ADMIN,       // Represents an admin role
-    GYM_OWNER,   // Represents a gym owner role
-    CUSTOMER     // Represents a customer role
-}
